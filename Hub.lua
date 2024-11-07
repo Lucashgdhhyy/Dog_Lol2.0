ebc026c73668d302ff8c247dc59c9d97a375438a
Lucashgdhhyy <155470121+Lucashgdhhyy@users.noreply.github.com>
2024-11-07 16:16:32 -0400

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,35 +1,27 @@
--- Why you are reading my code?
-
-local ArrayField = loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucashgdhhyy/Dog_Lol2.0/refs/heads/main/arrayfield/arrayfield.lua'))()
-local Window = ArrayField:CreateWindow({
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+setclipboard("https://rkns.link/xgbkl")
+local Window = Rayfield:CreateWindow({
    Name = "Dog_Lol | @dog_lolhub",
-   LoadingTitle = "Key Carregada!",
+   LoadingTitle = "Key System Completa",
    LoadingSubtitle = "by durk7896",
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "doglolfoldersaves"
+      FileName = "Durk1files"
    },
    Discord = {
       Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      Invite = "m4aZQFq5rt", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
       Title = "Dog_Lol | @dog_lolhub",
       Subtitle = "Key System",
-      Note = "Key System",
-      FileName = "doglolfolder2", -- It is recommended to use something unique as other scripts using ArrayField may overwrite your key file
+      Note = "Key System Copiada 3x mais longa",
+      FileName = "Durk2files", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like ArrayField to get the key from
-      Actions = {
-            [1] = {
-                Text = 'Click here for copy the key <---,
-                OnPress = function()  setclipboard("https://rkns.link/xgbkl")
-                end,
-                }
-            },
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"https://raw.githubusercontent.com/qfbteam/Dog_Lol/refs/heads/main/Key/key.txt", "@dog_lolperm"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
