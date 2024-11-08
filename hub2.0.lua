8cbca94c700c259b15888370a88d4d0c4ac337ca
Lucashgdhhyy <155470121+Lucashgdhhyy@users.noreply.github.com>
2024-11-08 12:25:34 -0400

    Update hub2.0.lua

--- hub2.0.lua
+++ hub2.0.lua
@@ -1,10 +1,4 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Lucashgdhhyy/Dog_Lol2.0/refs/heads/main/lib_purple.lua')))()
 local Player = game.Players.LocalPlayer
-local Window = OrionLib:MakeWindow({Name = "Dog_Lol|Script Hub V0.2", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroText = "Carregando script"})
-local Clogs = Window:MakeTab({
-	Name = "Changelogs",
-	Icon = "rbxassetid://4483362458",
-	PremiumOnly = false
-})
-Clogs:AddLabel("Bugs Fix")
-Clogs:AddParagraph("Beta v0.2","Fixing the color and hub ui")
+local Window = OrionLib:MakeWindow({Name = "Dog_Lol | @dog_lolhub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroText = "Carregando script"})
+
